1f59d46be902371c7a2a358f6b695eaacdc8fadf
Thrive Coder <devridwan1@gmail.com>
2023-10-15 07:58:52 +0100

    change the multer storage to memoryStorage and not disk storage

--- CONTROLLER/productController.js
+++ CONTROLLER/productController.js
@@ -1,18 +1,20 @@
 const Product = require("../MODEL/productModel");
 const multer = require("multer");
 
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/img/products");
-  },
-  filename: (req, file, cb) => {
-    const extension = file.mimetype.split("/")[1];
-    cb(
-      null,
-      `product-${req.body.name.replace(/ /g, "")}-${Date.now()}.${extension}`
-    );
-  },
-});
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "public/img/products");
+//   },
+//   filename: (req, file, cb) => {
+//     const extension = file.mimetype.split("/")[1];
+//     cb(
+//       null,
+//       `product-${req.body.name.replace(/ /g, "")}-${Date.now()}.${extension}`
+//     );
+//   },
+// });
+
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith("image")) {
