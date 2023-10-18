1319cdb7fa2841a50c5bfd2955a478d99eb236ed
Thrive Coder <devridwan1@gmail.com>
2023-10-18 23:17:06 +0100

    testing error on stage enviroment 2

--- CONTROLLER/authController.js
+++ CONTROLLER/authController.js
@@ -129,6 +129,8 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith("Bearer")
   ) {
     token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
 
   if (!token) {
@@ -162,10 +164,10 @@ exports.protect = catchAsync(async (req, res, next) => {
 });
 
 exports.isLoggedIn = async (req, res, next) => {
-  if (req.cookie.jwt) {
+  if (req.cookies.jwt) {
     try {
       const decoded = await promisify(jwt.verify)(
-        req.cookie.jwt,
+        req.cookies.jwt,
         process.env.JWT_SECRET
       );
 
