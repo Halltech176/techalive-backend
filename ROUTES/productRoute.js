40b90c30aecd73bba6b5fd6033e5992f92d61070
Thrive Coder <devridwan1@gmail.com>
2023-10-18 16:37:53 +0100

    testing bug on stage enviroment 1

--- ROUTES/productRoute.js
+++ ROUTES/productRoute.js
@@ -6,6 +6,8 @@ const router = express.Router();
 
 router.post(
   "/add-product",
+  authController.protect,
+  authController.restrictTo,
   productController.uploadProductImage,
   productController.resizeProductImage,
   productController.postProduct
